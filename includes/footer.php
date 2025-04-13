626df3b391cf477ceb2e5e4a93bb0791f9912c4d
ANDRI SAEFUL ADNAN <61642295+andrisaefuladnan@users.noreply.github.com>
2025-04-13 12:10:53 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="footer-content">
+        <p>&copy; 2025 SD Ihsaniyah Gajahmada. All Rights Reserved.</p>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<header class="header">
+<link rel="stylesheet" href="assets/css/style.css">
+<script src="assets/js/menu.js" defer></script>
+    <div class="logo">
+        <img src="assets/images/logo.png" alt="Logo" class="logo-img">
+        <span class="school-name" id="school-name">SD Ihsaniyah Gajahmada</span>
+    </div>
+    <nav>
+        <!-- Icon Menu untuk Ponsel -->
+        <div class="menu-icon" id="menu-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <ul class="nav-links" id="nav-links">
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="kontak.php">Kontak</a></li>
+            <li><a href="legalisir.php">Legalisir</a></li>
+            <li><a href="admin/index.php">Admin</a></li>
+        </ul>
+    </nav>
+</header>
